eca7b0931c19852ba1a89e7cc7bda9cc612bba46
Daniel Ribas <ds_ribas@hotmail.com>
2024-09-27 17:09:14 -0300

    chore: create storybook app

---
+++ app/storybook/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:storybook/main.directories.g.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+void main() {
+  runApp(const WidgetbookApp());
+}
+
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      // The [directories] variable does not exist yet,
+      // it will be generated in the next step
+      directories: directories,
+    );
+  }
+}
