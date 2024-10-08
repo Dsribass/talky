4ff84b8efb8034a9045dbae7c536837bd1da82e4
Daniel Ribas <ds_ribas@hotmail.com>
2024-10-08 09:51:10 -0300

    chore: add result_dart package and update analysis_options.yaml

--- libraries/core/lib/core.dart
+++ libraries/core/lib/core.dart
@@ -5,6 +5,7 @@ export 'package:bloc/bloc.dart';
 export 'package:dio/dio.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:go_router/go_router.dart';
+export 'package:result_dart/result_dart.dart';
 export 'package:rxdart/rxdart.dart';
 
 export 'src/env/environment.dart';
