1e5fd5ddaaa20cee4b79f389b246a779ef4d623e
Daniel Ribas <ds_ribas@hotmail.com>
2024-10-17 11:41:14 -0300

    refactor(core): update environment structure

--- libraries/core/lib/src/env/environment.dart
+++ libraries/core/lib/src/env/environment.dart
@@ -7,9 +7,30 @@ enum FlavorEnv {
   prod,
 }
 
-class Environment {
-  Environment._();
+/// Global environment instance
+///
+/// Ensure the environment configuration is loaded before accessing
+/// any environment variables.
+/// This can be achieved by calling the [Environment.init] method in the
+/// main function.
+const env = Environment._();
 
+class Environment {
+  const Environment._();
+
+  /// Initializes the environment configuration
+  ///
+  /// This method loads the appropriate environment configuration file
+  /// based on the [FlavorEnv] provided.
+  ///
+  /// It should be called before the application is run. For example:
+  ///
+  /// ```dart
+  /// void main() async {
+  ///   await Environment.init(FlavorEnv.dev);
+  ///   runApp(MyApp());
+  /// }
+  /// ```
   static Future<Environment> init(FlavorEnv flavor) async {
     final file = switch (flavor) {
       FlavorEnv.dev => '.env.dev',
@@ -18,6 +39,6 @@ class Environment {
 
     await dotenv.load(fileName: 'packages/core/$file');
 
-    return Environment._();
+    return const Environment._();
   }
 }
